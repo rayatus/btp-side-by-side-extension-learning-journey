55ccaa4a7e0beb441200a2fe5a4a9c2405f0d064
Jorge Sancho Royo <jsr@trifork.com>
2023-08-08 14:42:21 +0000

    improve BP API event-Handler

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -10,6 +10,8 @@ module.exports = cds.service.impl(async function () {
 
 
     const findCriticallity = (risk) => {
+        if (!risk) return 3
+
         const criticallityConfig = [
             {
                 upto: -Infinity,
@@ -53,35 +55,60 @@ module.exports = cds.service.impl(async function () {
     * Set criticality after a READ operation on /risks
     */
     this.after("READ", Risks, (data) => {
-        const risks = Array.isArray(data) ? data : [data];
-        risks.forEach((risk) => {
-            risk.criticality = findCriticallity(risk)
-        });
-    });
+        if (data){
+            const risks = Array.isArray(data) ? data : [data];
+            risks.forEach((risk) => {
+                risk.criticality = findCriticallity(risk)
+            })
+        }
+    })
 
-    
-  
+
+
     /**
     * Event Handler which triggers an external API and then calculates a field not exposed by that API
     * at HTTP Header there is APIKEY which is mandatory for consuming SAP API HUB External Services
     * 
     */
-        this.on("READ", BusinessPartners, async (req) => {
-            const BPsrv = await cds.connect.to("API_BUSINESS_PARTNER");
+    this.on("READ", BusinessPartners, async (req) => {
+        const BPsrv = await cds.connect.to("API_BUSINESS_PARTNER");
+        const { A_BusinessPartner } = BPsrv.entities
 
-            req.query.from(BPsrv.entities().A_BusinessPartner.name)          //--> specify which Entity from External service to consume
-            req.query.where("LastName <> '' and FirstName <> '' ")
-            req.query.columns(["BusinessPartner", "LastName", "FirstName"])
-    
-            const results = await BPsrv.transaction(req).send({
-                query: req.query,
-                headers: {
-                    apikey: process.env.apikey,
-                },
-            });
-            return results.map( bp => ({
-                    ...bp,
-                    FullName : [bp.FirstName, bp.LastName].join(" ")
-            }))
+        /* 
+        req.query.from(A_BusinessPartner.name)          //--> specify which Entity from External service to consume
+        req.query.where("LastName <> '' and FirstName <> '' ")
+        req.query.columns(["BusinessPartner", "LastName", "FirstName"])
+        */
+
+        //Let's build our own specific QUERY so that we do not inherit CAP standard properties within it
+        const qry = SELECT.from(A_BusinessPartner.name)
+            .where("LastName <> '' and FirstName <> '' ")
+            .columns(["BusinessPartner", "LastName", "FirstName"])
+            .limit(req.query.SELECT.limit)
+
+        const results = await BPsrv.transaction(qry).send({
+            query: qry,
+            headers: {
+                apikey: process.env.apikey,
+            },
         })
+        
+        //delete N initial rows for skipping them as API is a service which does not implements $SKIP / $TOP
+        if (req.query.SELECT.limit.offset){
+            for (let i = 0; i < req.query.limit.offset; i++) {
+                results.shift()
+            }
+        }
+
+        let data = results.map(bp => ({
+            ...bp,
+            FullName: [bp.FirstName, bp.LastName].join(" ")
+        }));
+
+        if (req.query.SELECT.count){
+            data.$count = data.length
+        }
+
+        return data
+    })
 });
