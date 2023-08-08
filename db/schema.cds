29b50cca145699660f168a6f27781eea092bb235
Jorge Sancho Royo <jsr@trifork.com>
2023-08-08 12:47:18 +0000

    extend external BP API with calculated field

--- db/schema.cds
+++ db/schema.cds
@@ -1,30 +1,32 @@
 namespace riskmanagement;
 
-using { managed } from '@sap/cds/common';
-// using an external service from S/4
-using { API_BUSINESS_PARTNER as external } from '../srv/external/API_BUSINESS_PARTNER.csn';
+using {managed} from '@sap/cds/common';
 
-entity Risks: managed {
-    key ID : UUID @(Core.Computed : true );
-    title : String(100);
-    owner : String;
-    prio : String(5);
-    descr : String;
-    miti : Association to Mitigations;
-    impact : Integer;
-    criticality : Integer;
+entity Risks : managed {
+    key ID          : UUID @(Core.Computed: true);
+        title       : String(100);
+        owner       : String;
+        prio        : String(5);
+        descr       : String;
+        miti        : Association to Mitigations;
+        impact      : Integer;
+        criticality : Integer;
+        bp          : Association to BusinessPartners;
 }
 
 entity Mitigations : managed {
-    key ID : UUID @(Core.Computed : true);
-    descr : String;
-    owner : String;
-    timeline : String;
-    risks : Association to many Risks on risks.miti = $self;
+    key ID       : UUID @(Core.Computed: true);
+        descr    : String;
+        owner    : String;
+        timeline : String;
+        risks    : Association to many Risks
+                       on risks.miti = $self;
 }
 
-entity BusinessPartners as projection on external.A_BusinessPartner {
-   key BusinessPartner,
-   LastName, 
-   FirstName 
-} 
+
+entity BusinessPartners {
+    key BusinessPartner : String;
+        LastName        : String;
+        FirstName       : String;
+        FullName        : String;
+}
--- srv/risk-service.js
+++ srv/risk-service.js
@@ -59,21 +59,29 @@ module.exports = cds.service.impl(async function () {
         });
     });
 
-    const BPsrv = await cds.connect.to("API_BUSINESS_PARTNER");
+    
+  
     /**
-    * Event-handler for read-events on the BusinessPartners entity.
-    * Each request to the API Business Hub requires the apikey in the header.
+    * Event Handler which triggers an external API and then calculates a field not exposed by that API
+    * at HTTP Header there is APIKEY which is mandatory for consuming SAP API HUB External Services
+    * 
     */
-    this.on("READ", BusinessPartners, async (req) => {
-        // The API Sandbox returns alot of business partners with empty names.
-        // We don't want them in our application
-        req.query.where("LastName <> '' and FirstName <> '' ");
+        this.on("READ", BusinessPartners, async (req) => {
+            const BPsrv = await cds.connect.to("API_BUSINESS_PARTNER");
 
-        return await BPsrv.transaction(req).send({
-            query: req.query,
-            headers: {
-                apikey: process.env.apikey,
-            },
-        });
-    });
+            req.query.from(BPsrv.entities().A_BusinessPartner.name)          //--> specify which Entity from External service to consume
+            req.query.where("LastName <> '' and FirstName <> '' ")
+            req.query.columns(["BusinessPartner", "LastName", "FirstName"])
+    
+            const results = await BPsrv.transaction(req).send({
+                query: req.query,
+                headers: {
+                    apikey: process.env.apikey,
+                },
+            });
+            return results.map( bp => ({
+                    ...bp,
+                    FullName : [bp.FirstName, bp.LastName].join(" ")
+            }))
+        })
 });
