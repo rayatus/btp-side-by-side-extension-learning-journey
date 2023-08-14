042b1874d5f79437e8a877cbcbd0b5e9045deb52
Jorge Sancho Royo <jsr@trifork.com>
2023-08-14 08:47:10 +0000

    implement expand BP at Risks

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -1,16 +1,26 @@
 // Imports
 const cds = require("@sap/cds");
+const req = require("express/lib/request");
 
 /**
 * The service implementation with all service handlers
 */
 module.exports = cds.service.impl(async function () {
     // Define constants for the Risk and BusinessPartners entities from the risk-service.cds file
-    const { Risks, BusinessPartners } = this.entities;
+    const { Risks, BusinessPartners } = this.entities
+    const BPsrv = await cds.connect.to("API_BUSINESS_PARTNER")
+    const { A_BusinessPartner } = BPsrv.entities
 
+    const businessPartner_CalculateCustomFields = (bp) => ({
+        ...bp,
+        FullName: [bp?.FirstName, bp?.LastName].join(" ")
+    })
 
     const findCriticallity = (risk) => {
-        if (!risk) return 3
+        
+        if (!risk?.impact){
+            return undefined
+        }
 
         const criticallityConfig = [
             {
@@ -47,15 +57,15 @@ module.exports = cds.service.impl(async function () {
         const index = criticallityConfig.findIndex(({ upto }) => {
             return upto > risk.impact
         })
-
-        return criticallityConfig[index - 1].criticallity
+        
+        return criticallityConfig[index - 1]?.criticallity
     }
 
     /**
     * Set criticality after a READ operation on /risks
     */
     this.after("READ", Risks, (data) => {
-        if (data){
+        if (data) {
             const risks = Array.isArray(data) ? data : [data];
             risks.forEach((risk) => {
                 risk.criticality = findCriticallity(risk)
@@ -63,7 +73,56 @@ module.exports = cds.service.impl(async function () {
         }
     })
 
+    /**
+    * Event-handler on risks.
+    * Retrieve BusinessPartner data from the external API
+    */
+    this.on("READ", Risks, async (req, next) => {
+        /*
+          Check whether the request wants an "expand" of the business partner
+          As this is not possible, the risk entity and the business partner entity are in different systems (SAP BTP and S/4 HANA Cloud), 
+          if there is such an expand, remove it
+        */
+        if (!req.query.SELECT.columns) return next();
 
+        const expandIndex = req.query.SELECT.columns.findIndex(
+            ({ expand, ref }) => expand && ref[0] === "bp"
+        );
+        console.log(req.query.SELECT.columns);
+        if (expandIndex < 0) return next();
+
+        req.query.SELECT.columns.splice(expandIndex, 1);
+        if (
+            !req.query.SELECT.columns.find((column) =>
+                column.ref.find((ref) => ref == "bp_BusinessPartner")
+            )
+        ) {
+            req.query.SELECT.columns.push({ ref: ["bp_BusinessPartner"] });
+        }
+
+        /*
+          Instead of carrying out the expand, issue a separate request for each business partner
+          This code could be optimized, instead of having n requests for n business partners, just one bulk request could be created
+        */
+        try {
+            res = await next();
+            res = Array.isArray(res) ? res : [res];
+
+            await Promise.all(
+                res.map(async (risk) => {
+                    const bp = await BPsrv.transaction(req).send({
+                        query: SELECT.one(A_BusinessPartner.name)
+                            .where({ BusinessPartner: risk.bp_BusinessPartner })
+                            .columns(["BusinessPartner", "FirstName", "LastName"]),
+                        headers: {
+                            apikey: process.env.apikey,
+                        },
+                    });
+                    risk.bp = businessPartner_CalculateCustomFields(bp);
+                })
+            );
+        } catch (error) { }
+    });
 
     /**
     * Event Handler which triggers an external API and then calculates a field not exposed by that API
@@ -71,8 +130,8 @@ module.exports = cds.service.impl(async function () {
     * 
     */
     this.on("READ", BusinessPartners, async (req) => {
-        const BPsrv = await cds.connect.to("API_BUSINESS_PARTNER");
-        const { A_BusinessPartner } = BPsrv.entities
+        
+        
 
         /* 
         req.query.from(A_BusinessPartner.name)          //--> specify which Entity from External service to consume
@@ -92,20 +151,17 @@ module.exports = cds.service.impl(async function () {
                 apikey: process.env.apikey,
             },
         })
-        
+
         //delete N initial rows for skipping them as API is a service which does not implements $SKIP / $TOP
-        if (req.query.SELECT.limit.offset){
+        if (req.query.SELECT.limit.offset) {
             for (let i = 0; i < req.query.limit.offset; i++) {
                 results.shift()
             }
         }
 
-        let data = results.map(bp => ({
-            ...bp,
-            FullName: [bp.FirstName, bp.LastName].join(" ")
-        }));
+        const data = results.map(bp => businessPartner_CalculateCustomFields(bp));
 
-        if (req.query.SELECT.count){
+        if (req.query.SELECT.count) {
             data.$count = data.length
         }
 
